4dca29b6171bdb23584e881a3ff724468c7a8533
Elivelton Alves <elivelton.valdeiralves92@gmail.com>
2022-06-30 15:05:07 -0300

    Ajustando o tamanho da tela do cypress

--- cypress/e2e/home.cy.js
+++ cypress/e2e/home.cy.js
@@ -2,7 +2,7 @@
 //acessar o site para teste
 describe('home page', ()=>{
     it('app deve estar online', ()=>{
-        cy.viewport(1440, 900) //definir o tamanho da resolução que o cypress vai abrir
+        cy.viewport(1280, 720) //definir o tamanho da resolução que o cypress vai abrir
         cy.visit('https://buger-eats-qa.vercel.app') 
         cy.get('#page-home main h1').should('have.text', 'Seja um parceiro entregador pela Buger Eats') //verificar se existe um texto
     })
